c57449a49e18370654bb177e80bfd6246982f3ca
Robert <robstearn@me.com>
2012-05-07 18:53:15 +0100

    Initial Commit

---
+++ MacBarcode/RSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RSAppDelegate.h
+//  MacBarcode
+//
+//  Created by Robert Stearn on 07.05.12.
+//  Copyright (c) 2012 Cocoadelica. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface RSAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ MacBarcode/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  MacBarcode
+//
+//  Created by Robert Stearn on 07.05.12.
+//  Copyright (c) 2012 Cocoadelica. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+	return NSApplicationMain(argc, (const char **)argv);
+}
